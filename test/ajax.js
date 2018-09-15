3f6a622948b38451e8d8cb084f2d28d43dd8e572
Vasyl Kurtash <vasyl.kurtash@gmail.com>
2018-09-15 23:21:21 +0300

    add ajax to homepage

---
+++ test/ajax.js
@@ -0,0 +1,14 @@
+
+// var xhr = new XMLHttpRequest();
+
+// xhr.open('GET', 'phones.json', false);
+// xhr.send();
+
+// xhr.onreadystatechange = function() {
+// 	if (xhr.readyState != 4) return;
+// 	if (xhr.status != 200) {
+// 		console.log(xhr.status + ':' + xhr.statusText);
+// 	} else {
+// 		console.log(xhr.responseText);
+// 	}
+// }
