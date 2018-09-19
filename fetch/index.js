75a9368ef8008e8b537c3a8d88aa4c04b3ef2f8b
Vasyl Kurtash <vasyl.kurtash@gmail.com>
2018-09-20 00:22:51 +0300

    fetch configuration

--- fetch/index.js
+++ fetch/index.js
@@ -33,7 +33,50 @@ function touch() {
 		});
 // }
 
-fetch('https://api.github.com/users/swapnilbangare')
-	.then((res) => res.json())
-	.then((data) => console.log(data))
-	.catch((err) => console.log('Something went wrong', err));
+// function fetch1(){
+		var hd = new Headers({'Content-Type': 'application/json'});
+
+		var int = {
+			method: 'GET',
+			headers: hd
+		}
+
+		var req = new Request('https://api.github.com/users/swapnilbangare', int);
+
+		fetch(req)
+			.then((res) => res.json())
+			.then((data) => console.log(data))
+			.catch((err) => console.log('Something went wrong', err));
+// }
+
+// --------------------------------------------------
+// https://developer.mozilla.org/ru/docs/Web/API/Fetch_API/Using_Fetch
+
+var myImage = document.querySelector('.my-image');
+
+var myHeaders = new Headers();
+
+var myInit = {
+	method: "GET",
+	headers: myHeaders,
+	mode: 'cors',
+	cache: 'default'
+}
+
+var myRequest = new Request('https://upload.wikimedia.org/wikipedia/commons/7/77/Delete_key1.jpg', myInit);
+
+fetch(myRequest)
+// fetch('https://sraka')
+	.then(res => {
+		if(res.ok) {
+			return res.blob()
+		}
+		throw new Error('Network response was not ok');
+	})
+	.then(res => {
+		  console.log(res)
+		var objectURL = URL.createObjectURL(res);
+		  console.log(objectURL);
+		myImage.src = objectURL;
+	})
+	.catch(err => console.log('There has been a problem with your fetch operator: ' + err.message));
